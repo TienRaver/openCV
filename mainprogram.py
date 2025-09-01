32279cbccfd54637cfabb39a885809d461cced8b
Tien Raver <tienraver@gmail.com>
2025-09-01 22:49:06 +0700

    xxx

--- mainprogram.py
+++ mainprogram.py
@@ -1,4 +1,15 @@
-import matplotlib_tool
-#folder_path = input("Folder path: ")
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
 image_path = input("Image path: ")
-matplotlib_tool.OpenImage(image_path)
+image = Image.open(image_path)
+grayscale_image = image.convert("L")
+#plt.imshow(grayscale_image,cmap="gray")
+image_array = np.array(grayscale_image)
+plt.figure(num="figure 1",figsize=(8,6),facecolor="palegoldenrod")
+plt.hist(image_array.flatten(),bins=128)
+plt.title("LUOC DO ANH XAM")
+plt.xlabel("Gia tri pixel")
+plt.ylabel("So luong")
+plt.show()
--- matplotlib_tool.py
+++ matplotlib_tool.py
@@ -1,5 +1,7 @@
 from PIL import Image
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 def OpenImage(image_path):
     image = Image.open(image_path)
-    plt.show(image)
+    plt.imshow(image)
+    plt.show()

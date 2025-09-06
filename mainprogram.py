d263f9469b4ffe96e121b1295e914f96e4b1429a
Tien Raver <tienraver@gmail.com>
2025-09-06 08:49:05 +0700

    xxx

--- mainprogram.py
+++ mainprogram.py
@@ -1,15 +1,16 @@
 from PIL import Image
-import matplotlib.pyplot as plt
 import numpy as np
+import os
+import matplotlib.pyplot as plt
+import matplotlib_tool
 
-image_path = input("Image path: ")
-image = Image.open(image_path)
-grayscale_image = image.convert("L")
-#plt.imshow(grayscale_image,cmap="gray")
-image_array = np.array(grayscale_image)
-plt.figure(num="figure 1",figsize=(8,6),facecolor="palegoldenrod")
-plt.hist(image_array.flatten(),bins=128)
-plt.title("LUOC DO ANH XAM")
-plt.xlabel("Gia tri pixel")
-plt.ylabel("So luong")
+image_folder = input("Image folder: ")
+image_folder = image_folder.replace("\\","/")
+list_folder = os.listdir(image_folder)
+lists = []
+for list in list_folder:
+    lists.append(os.path.join(image_folder,list))
+plt.figure(figsize=(6,6))
+plt.imshow(matplotlib_tool.image_avg(lists))
+plt.title("Averaging Image")
 plt.show()
--- matplotlib_tool.py
+++ matplotlib_tool.py
@@ -1,7 +1,36 @@
 from PIL import Image
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-def OpenImage(image_path):
+import numpy as np
+"""
+# Func 1: Can bang anh xam
+def histogram_equalization(image_path):
     image = Image.open(image_path)
-    plt.imshow(image)
-    plt.show()
+    # Chuyen sang anh xam
+    if image.mode != "L":
+        image = image.convert("L")
+    # Numpy array & histogram
+    image_array = np.array(image)
+    # Phan phoi tich luy CDF - Can bang anh xam
+    histogram,bins = np.histogram(image_array,bins=256,range=(0,256),density=True)
+    cdf = histogram.cumsum()
+    cdf = 255*cdf/cdf[-1]
+    image_equal = np.interp(image_array,bins[:-1],cdf)
+    image_equalized = Image.fromarray(image_equal.astype("uint8"))
+    # Tra ve anh da can bang xam
+    return image_equalized
+"""
+
+# Func 2: Tinh trung binh anh
+def image_avg(lists):
+    # Tao mang ban dau
+    total_array = np.array(Image.open(lists[0]),"f")
+    count = 1
+    # Tinh trung binh mang
+    for list in lists[1:]:
+        total_array += np.array(Image.open(list),"f")
+        count += 1
+    image_array = total_array/count
+    # Tao anh tu mang trung binh
+    image_average = Image.fromarray(image_array.astype("uint8"))
+    # Tra ve dau ra
+    return image_average
